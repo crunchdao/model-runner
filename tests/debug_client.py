b0e2e78e1788db1fdcd3d8c513be296736a3e6c0
Boris Nieuwenhuis <borisnieuwenhuis@gmail.com>
2025-09-22 13:05:40 +0200

    improve usage

--- tests/debug_client.py
+++ tests/debug_client.py
@@ -11,7 +11,7 @@ from model_runner.grpc.generated.dynamic_subclass_pb2 import SetupRequest, CallR
 
 from model_runner.utils.datatype_transformer import encode_data, decode_data
 
-SERVER_ADDRESS_PHALA = "d175dfa1b5cbb22438a52e2b6f1b5464c0294b36-%sg.dstack-pha-prod10.phala.network"
+
 SERVER_ADDRESS = "localhost:50051"
 
 # @pytest.mark.asyncio
@@ -150,12 +150,16 @@ def test_health_call():
         resp = stub.Check(health_pb2.HealthCheckRequest(service=""), timeout=0.5)
         assert resp.status == health_pb2.HealthCheckResponse.SERVING
 
+SERVER_ADDRESS_PHALA = "d175dfa1b5cbb22438a52e2b6f1b5464c0294b36-%sg.dstack-pha-prod10.phala.network"
+
 def test_health_call_phala():
-    with open("phala_gateway.crt", "rb") as f:
+    with open("tests/phala_gateway.crt", "rb") as f:
         trusted_certs = f.read()
 
     creds = grpc.ssl_channel_credentials(root_certificates=trusted_certs)
-    with grpc.secure_channel(SERVER_ADDRESS_PHALA % 5001, credentials=creds) as channel:
+    server_address = SERVER_ADDRESS_PHALA % "50002"
+    print(f"Connecting to {server_address}")
+    with grpc.secure_channel(server_address, credentials=creds) as channel:
         stub = health_pb2_grpc.HealthStub(channel)
         resp = stub.Check(health_pb2.HealthCheckRequest(service=""), timeout=10.0)
         assert resp.status == health_pb2.HealthCheckResponse.SERVING
