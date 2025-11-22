3279b57404c0cdd29eeb4bbc39de4c287bfb340e
boutrig abdennour <abdennour.boutrig@crunchdao.com>
2025-11-22 15:01:14 +0100

    change(grpc): increase message length to 64MB by default

--- model_runner/server.py
+++ model_runner/server.py
@@ -12,7 +12,6 @@ from .servicers.train_infer_servicer import TrainInferStreamServicer
 
 logger = logging.getLogger('model_runner')
 logging.basicConfig(level=logging.INFO, format="%(levelname)-8s - %(message)s")
-
 @click.command()
 @click.option('--address', default='[::]:50051', envvar='GRPC_ADDRESS', help='IP + Port of server GRPC.')
 @click.option('--code-directory', type=click.Path(exists=True, file_okay=False), envvar='CODE_DIRECTORY', default='/workspace/submission/code')
@@ -20,13 +19,17 @@ logging.basicConfig(level=logging.INFO, format="%(levelname)-8s - %(message)s")
 @click.option('--has-gpu', type=bool, envvar='HAS_GPU', default=False, help='Information if GPU is available')
 @click.option('--main-file', envvar='MAIN_FILE', default='main.py', help="main file's name of model")
 @click.option('--log-level', type=click.Choice(["debug", "info", "warning", "error", "critical"], case_sensitive=False), default='info', envvar='LOG_LEVEL', help='Logging level')
+@click.option('--max-send-message-length', type=int, envvar='GRPC_MAX_SEND_MESSAGE_LENGTH', default=64 * 1024 * 1024, help='Maximum send message length in bytes for gRPC server')
+@click.option('--max-receive-message-length', type=int, envvar='GRPC_MAX_RECEIVE_MESSAGE_LENGTH', default=64 * 1024 * 1024, help='Maximum receive message length in bytes for gRPC server')
 def cli(
     address: str,
     code_directory: str,
     resource_directory: str,
     has_gpu: str,
     main_file: str,
-    log_level: str
+    log_level: str,
+    max_send_message_length: int,
+    max_receive_message_length: int
 ):
     """Program giving access remotely to model via RPC"""
 
@@ -34,7 +37,13 @@ def cli(
 
     # Use at least 2 workers to ensure Health checks are always responsive,
     # since the other service methods are restricted to one concurrent call
-    server = grpc.server(ThreadPoolExecutor(max_workers=2))
+    server = grpc.server(
+        ThreadPoolExecutor(max_workers=2),
+        options=[
+            ('grpc.max_send_message_length', max_send_message_length),
+            ('grpc.max_receive_message_length', max_receive_message_length)
+        ]
+    )
 
     health_servicer = health.HealthServicer(
         experimental_non_blocking=True,
